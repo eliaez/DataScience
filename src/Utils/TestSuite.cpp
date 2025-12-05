e8a363a12c84ab15601df4c4227ff8e9e4cef9e0
Romain Godard <romaingodard07@yahoo.fr>
2025-12-05 12:01:32 +0100

    TestSuite

---
+++ src/Utils/TestSuite.cpp
@@ -0,0 +1,22 @@
+#include "Utils/TestSuite.hpp"
+#include <iostream>
+
+namespace TestSuite {
+
+void Tests::run_all() {
+
+    for (auto& f : to_test) {
+        try {
+            f.first();
+
+            std::cout << "✓ " << f.second << std::endl;
+            success++;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "✗ " << f.second << ": " << e.what() << std::endl;
+            failure++;
+        }
+    }
+    std::cout << "\n" << "Tests Results: " << success << "/" << success+failure << std::endl;
+}
+}
