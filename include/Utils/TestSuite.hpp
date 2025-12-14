3819e127e44178d6e2ff125057272d83f5ed0692
Romain Godard <romaingodard07@yahoo.fr>
2025-12-14 18:22:04 +0100

    Add test methods and datasets

--- include/Utils/TestSuite.hpp
+++ include/Utils/TestSuite.hpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <string>
 #include <stdexcept>
+#include <iomanip>
+#include <sstream>
 
 namespace TestSuite {
 
@@ -35,4 +37,29 @@ namespace TestSuite {
             ); \
         } \
     } while(0); 
+
+#define ASSERT_EQ_INT(actual, expected) \
+    do { \
+        auto _actual = static_cast<int>(actual); \
+        auto _expected = static_cast<int>(expected); \
+        if (_actual != _expected) { \
+            throw std::runtime_error( \
+                std::string("Ligne ") + std::to_string(__LINE__) \
+            ); \
+        } \
+    } while(0);
+
+#define ASSERT_EQ_SCI3(actual, expected) \
+    do { \
+        auto _actual = (actual); \
+        auto _expected = (expected); \
+        std::ostringstream oss_a, oss_e; \
+        oss_a << std::scientific << std::setprecision(3) << _actual; \
+        oss_e << std::scientific << std::setprecision(3) << _expected; \
+        if (oss_a.str() != oss_e.str()) { \
+            throw std::runtime_error( \
+                std::string("Ligne ") + std::to_string(__LINE__) \
+            ); \
+        } \
+    } while(0);
     
