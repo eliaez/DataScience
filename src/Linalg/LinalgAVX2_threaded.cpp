fd18ba4afa3b8443e7fc2fe25d6e1bdfe8b361cd
Romain Godard <romaingodard07@yahoo.fr>
2026-01-16 12:32:50 +0100

    LU_decompo and solveLU

--- src/Linalg/LinalgAVX2_threaded.cpp
+++ src/Linalg/LinalgAVX2_threaded.cpp
@@ -6,6 +6,324 @@ namespace Linalg {
 namespace AVX2_threaded {
 #ifdef __AVX2__
 
+std::tuple<int, std::vector<double>, std::vector<double>> LU_decomposition(const Dataframe& df) {
+
+    int nb_swaps = 0;
+    size_t n = df.get_cols();
+    std::vector<double> LU = df.get_data();
+
+    // Permutation matrix is Id at first
+    std::vector<double> swaps(n*n, 0.0);
+    for (size_t i = 0; i < n; i++) {
+        swaps[i*n + i] = 1;
+    }
+
+    for (size_t k = 0; k < n-1; k++) {
+
+        // Partial pivot (get most important pivot and permutate lines)
+        auto [max, idx] = std::tuple{LU[k*n + k], k};
+        for (size_t i = k+1; i < n; i++) {
+
+            double val = std::abs(LU[k*n + i]);
+            if (max < val) {
+                max =  val;
+                idx = i;
+            }
+        }
+
+        // Permutation of rows
+        if (k != static_cast<size_t>(idx)) {
+            for (size_t j = 0; j < n; j++) {
+                std::swap(LU[j*n + k], LU[j*n + idx]);
+                std::swap(swaps[j*n + k], swaps[j*n + idx]);
+            }
+            nb_swaps++;
+        }
+
+        // Pivot
+        double p = LU[k*n + k];
+        if (std::abs(p) < 1e-14) {
+            // det = 0
+            throw std::runtime_error("Singular Matrix <=> Det = 0");
+        }
+
+        // LU decomposition algorithm by blocks AVX2
+        size_t vec_size = n - ((n - k - 1) % NB_DB);
+
+        // After multiple tests a minimum was decided
+        if (n > 512 && n - k - 1 > 8) {
+            // ThreadPool Variables
+            ThreadPool& pool = ThreadPool::instance();
+            size_t nb_threads = pool.nb_threads;
+
+            std::vector<std::future<void>> futures;
+            futures.reserve(nb_threads);
+
+            size_t chunk = (int)((n - k - 1) / nb_threads);
+            size_t start = k+1, end = k + 1 + chunk;
+
+            for (size_t nb = 0; nb < nb_threads; nb++) {
+                if (nb+1 == nb_threads) end = n;
+
+                auto fut = pool.enqueue([start, end, k, n, p, vec_size, &LU] {
+                    for (size_t i = start; i < end; i++) {
+
+                        double Lvalue = LU[k*n + i] / p;
+                        LU[k*n + i] = Lvalue;
+                        
+                        // Broadcast our L value in vector
+                        __m256d vec_Lvalue = _mm256_set1_pd(Lvalue);
+                        
+                        // Update value in other cols 
+                        // Block version
+                        size_t j = k+1;
+                        for (; j < vec_size; j+=NB_DB) {
+                                    
+                            __m256d vec_pivot = _mm256_set_pd(
+                                LU[(j+3)*n + k],
+                                LU[(j+2)*n + k],
+                                LU[(j+1)*n + k],
+                                LU[j*n + k]
+                            );
+                            
+                            __m256d vec_LU = _mm256_set_pd(
+                                LU[(j+3)*n + i],
+                                LU[(j+2)*n + i],
+                                LU[(j+1)*n + i],
+                                LU[j*n + i]
+                            );
+                            
+                            // (a*b) - c
+                            vec_LU = _mm256_fnmadd_pd(vec_Lvalue, vec_pivot, vec_LU);
+
+                            alignas(32) double temp[4];
+                            _mm256_store_pd(temp, vec_LU);
+                            LU[j*n + i] = temp[0];
+                            LU[(j+1)*n + i] = temp[1];
+                            LU[(j+2)*n + i] = temp[2];
+                            LU[(j+3)*n + i] = temp[3];
+                        }
+
+                        // Scalar residual
+                        for (; j < n; j++) {
+                            LU[j*n + i] -= Lvalue * LU[j*n + k];
+                        }
+                    }
+                });
+                futures.push_back(std::move(fut));
+                start += chunk;
+                end += chunk;
+            }
+            for (auto& fut : futures) fut.wait();
+        } else {
+            for (size_t i = k+1; i < n; i++) {
+
+                double Lvalue = LU[k*n + i] / p;
+                LU[k*n + i] = Lvalue;
+                
+                // Broadcast our L value in vector
+                __m256d vec_Lvalue = _mm256_set1_pd(Lvalue);
+                
+                // Update value in other cols 
+                // Block version
+                size_t j = k+1;
+                for (; j < vec_size; j+=NB_DB) {
+                            
+                    __m256d vec_pivot = _mm256_set_pd(
+                        LU[(j+3)*n + k],
+                        LU[(j+2)*n + k],
+                        LU[(j+1)*n + k],
+                        LU[j*n + k]
+                    );
+                    
+                    __m256d vec_LU = _mm256_set_pd(
+                        LU[(j+3)*n + i],
+                        LU[(j+2)*n + i],
+                        LU[(j+1)*n + i],
+                        LU[j*n + i]
+                    );
+                    
+                    // (a*b) - c
+                    vec_LU = _mm256_fnmadd_pd(vec_Lvalue, vec_pivot, vec_LU);
+
+                    alignas(32) double temp[4];
+                    _mm256_store_pd(temp, vec_LU);
+                    LU[j*n + i] = temp[0];
+                    LU[(j+1)*n + i] = temp[1];
+                    LU[(j+2)*n + i] = temp[2];
+                    LU[(j+3)*n + i] = temp[3];
+                }
+
+                // Scalar residual
+                for (; j < n; j++) {
+                    LU[j*n + i] -= Lvalue * LU[j*n + k];
+                }
+            }
+        }
+    }
+    return std::make_tuple(nb_swaps, std::move(swaps), std::move(LU));
+}
+
+Dataframe solveLU_inplace(const std::vector<double>& perm, const std::vector<double>& LU, size_t n) {
+
+    // After multiple tests a minimum was decided
+    constexpr size_t THREADING_THRESHOLD = 512;
+    if (n < THREADING_THRESHOLD) {
+        return AVX2::solveLU_inplace(perm, LU, n);
+    }
+
+    std::vector<double> y(n*n, 0.0);
+    size_t prefetch_dist = NB_DB;
+
+    // Store the Diag
+    std::vector<double> diag_U(n);
+    for (size_t i = 0; i < n; i++) {
+        diag_U[i] = LU[i * n + i];
+    }
+
+    size_t vec_size = n - (n % NB_DB);
+
+    // ThreadPool Variables
+    ThreadPool& pool = ThreadPool::instance();
+    size_t nb_threads = pool.nb_threads;
+
+    std::vector<std::future<void>> futures;
+    futures.reserve(nb_threads);
+
+    size_t chunk = (int)(vec_size / nb_threads);
+    size_t start = 0, end = chunk;
+
+    for (size_t nb = 0; nb < nb_threads; nb++) {
+        if (nb+1 == nb_threads) end = vec_size;
+
+        auto fut = pool.enqueue([start, end, n, &perm, &y, &diag_U, &LU] {
+            
+            // By Blocks with AVX2
+            for (size_t k = start; k < end; k+=NB_DB) {
+
+                // Solving Ly = perm (No division because diag = 1)
+                // Forward substitution 
+                for (size_t i = 0; i < n; i++) {
+
+                    if (k + prefetch_dist < end) {
+                        for (size_t p = 0; p < NB_DB; p++) {
+                            _mm_prefetch((const char*)&perm[(k+p+prefetch_dist)*n + i], _MM_HINT_T0);
+                        }        
+                    }
+
+                    __m256d sum = _mm256_set_pd(
+                        perm[(k+3)*n + i],
+                        perm[(k+2)*n + i],
+                        perm[(k+1)*n + i],
+                        perm[(k+0)*n + i]
+                    );
+                    
+                    for (size_t j = 0; j < i; j++) {
+
+                        if (j + prefetch_dist < i) {
+                            for (size_t p = 0; p < NB_DB; p++) {
+                                _mm_prefetch((const char*)&y[(k+p)*n + j + prefetch_dist], _MM_HINT_T0);
+                            }
+                        }
+
+                        __m256d LU_val = _mm256_set1_pd(LU[j*n + i]);
+                        
+                        __m256d y_vals = _mm256_set_pd(
+                            y[(k+3)*n + j],
+                            y[(k+2)*n + j],
+                            y[(k+1)*n + j],
+                            y[(k+0)*n + j]
+                        );
+                        sum = _mm256_fnmadd_pd(LU_val, y_vals, sum);
+                    }
+                    
+                    double result[4];
+                    _mm256_storeu_pd(result, sum);
+
+                    y[(k+0)*n + i] = result[0];
+                    y[(k+1)*n + i] = result[1];
+                    y[(k+2)*n + i] = result[2];
+                    y[(k+3)*n + i] = result[3];
+                }
+
+                // Solving Ux = y 
+                // Backward substitution
+                for (int i = static_cast<int>(n)-1; i >= 0; i--) {
+
+                    __m256d sum = _mm256_set_pd(
+                        y[(k+3)*n + i],
+                        y[(k+2)*n + i],
+                        y[(k+1)*n + i],
+                        y[(k+0)*n + i]
+                    );
+                    
+                    for (size_t j = i+1; j < n; j++) {
+                        
+                        __m256d LU_vec = _mm256_set1_pd(LU[j*n + i]);
+
+                        __m256d y_vals = _mm256_set_pd(
+                            y[(k+3)*n + j],
+                            y[(k+2)*n + j],
+                            y[(k+1)*n + j],
+                            y[(k+0)*n + j]
+                        );
+                        
+                        sum = _mm256_fnmadd_pd(LU_vec, y_vals, sum);
+                    }
+                    
+                    double result[4];
+                    _mm256_storeu_pd(result, sum);
+                    
+                    y[(k+0)*n + i] = result[0];
+                    y[(k+1)*n + i] = result[1];
+                    y[(k+2)*n + i] = result[2];
+                    y[(k+3)*n + i] = result[3];
+
+                    for(size_t p = 0; p < NB_DB; p++) {
+                        if (std::abs(y[(k+p)*n + i]) < 1e-14) y[(k+p)*n + i] = 0;
+                        else y[(k+p)*n + i] /= diag_U[i];
+                    }
+                }
+            }
+        });
+        futures.push_back(std::move(fut));
+        start += chunk;
+        end += chunk;
+    }
+    for (auto& fut : futures) fut.wait();
+
+    // Scalar Residual for k 
+    for (size_t k = end; k < n; k++) {
+
+        // Solving Ly = perm (No division because diag = 1)
+        // Forward substitution
+        for (size_t i = 0; i < n; i++) {
+            
+            double sum = 0.0;
+            sum = perm[k*n + i];
+            for (size_t j = 0; j < i; j++) {
+                sum -= LU[j*n + i] * y[k*n + j];
+            }
+            y[k*n + i] = sum;
+        }
+
+        // Solving Ux = y 
+        // Backward substitution
+        for (int i = static_cast<int>(n)-1; i >= 0; i--) {
+
+            double sum = y[k*n + i];
+            for (size_t j = i+1; j < n; j++) {
+                sum -= LU[j*n + i] * y[k*n + j];
+            }
+            y[k*n + i] = sum;
+            if (std::abs(y[k*n + i]) < 1e-14) y[k*n + i] = 0;
+            else y[k*n + i] /= diag_U[i];
+            
+        }
+    }
+    return {n, n, false, std::move(y)};
+}
+
 double horizontal_red(__m256d& vec) {
     return AVX2::horizontal_red(vec);
 }
@@ -260,6 +578,170 @@ Dataframe transpose(Dataframe& df) {
     return {rows, cols, false, std::move(data), df.get_headers(), 
         df.get_encoder(), df.get_encodedCols()};
 }
+
+/*Dataframe inverse(Dataframe& df) {
+
+    auto [det, swaps, LU] = determinant(df);
+
+    size_t n = df.get_cols();
+    
+    // Id matrix
+    std::vector<double> id(n*n, 0.0);
+    for (size_t i = 0; i < n; i++) {
+        id[i*n + i] = 1;
+    }
+    Dataframe df_id = {n, n, false, std::move(id)};
+
+    // If no LU matrix was returned, then the matrix is triangular
+    if (LU.empty()) {
+        std::vector<double> y(n*n, 0.0);
+
+        // Diag
+        if (swaps[0] == 3) {
+            for (size_t i = 0; i < n; i++) {
+               y[i*n + i] = 1 / df.at(i*n+i);
+            }
+            return {n, n, false, std::move(y)};
+        }
+        // Up
+        else if (swaps[0] == 2) {
+
+            // Solving Uy = Id 
+            // Backward substitution by blocks
+            size_t k = 0;
+            size_t vec_size = n - (n % NB_DB);
+            for (; k < vec_size; k+=NB_DB) {
+                for (int i = static_cast<int>(n)-1; i >= 0; i--) {
+
+                    double diag_i = df.at(i*n + i);
+
+                    __m256d sum = _mm256_set_pd(
+                        df_id.at((k+3)*n + i),
+                        df_id.at((k+2)*n + i),
+                        df_id.at((k+1)*n + i),
+                        df_id.at((k+0)*n + i)
+                    );
+                    
+                    for (size_t j = i+1; j < n; j++) {
+                        
+                        __m256d LU_vec = _mm256_set1_pd(df.at(j*n + i));
+
+                        __m256d y_vals = _mm256_set_pd(
+                            y[(k+3)*n + j],
+                            y[(k+2)*n + j],
+                            y[(k+1)*n + j],
+                            y[(k+0)*n + j]
+                        );
+                        
+                        sum = _mm256_fnmadd_pd(LU_vec, y_vals, sum);
+                    }
+                    
+                    double result[4];
+                    _mm256_storeu_pd(result, sum);
+                    
+                    y[(k+0)*n + i] = result[0];
+                    y[(k+1)*n + i] = result[1];
+                    y[(k+2)*n + i] = result[2];
+                    y[(k+3)*n + i] = result[3];
+
+                    for(size_t p = 0; p < NB_DB; p++) {
+                        if (std::abs(y[(k+p)*n + i]) < 1e-14) y[(k+p)*n + i] = 0;
+                        else y[(k+p)*n + i] /= diag_i;
+                    }
+                }
+            }
+
+            // Scalar Residual for k 
+            for (; k < n; k++) {
+                for (int i = static_cast<int>(n)-1; i >= 0; i--) {
+
+                    y[k*n + i] = df_id.at(k*n + i);
+                    for (size_t j = i+1; j < n; j++) {
+                        y[k*n + i] -= df.at(j*n + i) * y[k*n + j];
+                    }
+                    if (std::abs(y[k*n + i]) < 1e-14) y[k*n + i] = 0;
+                    else y[k*n + i] /= df.at(i*n+i);
+                }
+            }
+            return {n, n, false, std::move(y)};
+        }
+        // Down
+        else {
+
+            // Solving Ly = Id
+            // Forward substitution by blocks with AVX2
+            size_t k = 0;
+            size_t vec_size = n - (n % NB_DB);
+
+            for (; k < vec_size; k+=NB_DB) {
+
+                for (size_t i = 0; i < n; i++) {
+
+                    double diag_i = df.at(i*n + i);
+
+                    __m256d sum = _mm256_set_pd(
+                        df_id.at((k+3)*n + i),
+                        df_id.at((k+2)*n + i),
+                        df_id.at((k+1)*n + i),
+                        df_id.at((k+0)*n + i)
+                    );
+                    
+                    for (size_t j = 0; j < i; j++) {
+
+                        __m256d LU_val = _mm256_set1_pd(df.at(j*n + i));
+                        
+                        __m256d y_vals = _mm256_set_pd(
+                            y[(k+3)*n + j],
+                            y[(k+2)*n + j],
+                            y[(k+1)*n + j],
+                            y[(k+0)*n + j]
+                        );
+                        sum = _mm256_fnmadd_pd(LU_val, y_vals, sum);
+                    }
+                    
+                    double result[4];
+                    _mm256_storeu_pd(result, sum);
+
+                    y[(k+0)*n + i] = result[0];
+                    y[(k+1)*n + i] = result[1];
+                    y[(k+2)*n + i] = result[2];
+                    y[(k+3)*n + i] = result[3];
+
+                    for(size_t p = 0; p < NB_DB; p++) {
+                        if (std::abs(y[(k+p)*n + i]) < 1e-14) y[(k+p)*n + i] = 0;
+                        else y[(k+p)*n + i] /= diag_i;
+                    }
+                }
+            }
+
+            // Scalar Residual for k
+            for (; k < n; k++) {
+                for (size_t i = 0; i < n; i++) {
+
+                    double sum = 0.0;
+                    sum = df_id.at(k*n + i);
+                    for (size_t j = 0; j < i; j++) {
+                        sum -= df.at(j*n + i) * y[k*n + j];
+                    }
+                    y[k*n + i] = sum / df.at(i*n + i);
+                }
+            }
+            return {n, n, false, std::move(y)};
+        }
+    }
+    else {
+        
+        // Permutation matrix
+        Dataframe df_swaps = {n, n, false, std::move(swaps)};
+        df_swaps.change_layout_inplace("AVX2_threaded");
+
+        // Row - Col to use multiply from AVX2
+        Dataframe perm = multiply(df_swaps, df_id); 
+
+        Dataframe res = solveLU_inplace(perm.get_data(), LU, n);
+        return res;
+    }
+}*/
 
 #endif
 }
