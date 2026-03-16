0da53cd953c916ef3b6f93f8cbbfe80319214c6b
Romain Godard <romaingodard07@yahoo.fr>
2026-03-16 23:09:00 +0100

    Refactor and end of detection methods for ARIMA and SARIMA

--- include/Stats/Time_series.hpp
+++ include/Stats/Time_series.hpp
@@ -2,77 +2,15 @@
 
 #include <vector>
 
-namespace Stats_TS {
-    class ARIMA {
-        protected:
-            int p_;
-            int d_;
-            int q_;
-            std::vector<double> y_diff;
-            std::vector<double> ar_coeffs;
-            std::vector<double> ma_coeffs;
-            std::vector<double> residuals;
+namespace Stats_TS {;
 
-        public:
-            // Set params to -1 for them to be automatically detected
-            ARIMA(int p = -1, int d = -1, int q = -1) :
-                p_(p), d_(d), q_(q) {};
+    // Function to detect and returns ARIMA params on your vector
+    // Will return p, d, q
+    std::vector<int> detect_ARIMA(const std::vector<double>& col);
 
-            // Function to detect and returns ARIMA params on your vector
-            // Will return p, d, q
-            virtual std::vector<int> detect(const std::vector<double>& col);
-
-            // Function to detect (if params still at -1) and fit ARIMA params on your vector
-            virtual void fit(const std::vector<double>& col);
-
-            // Function to predict based on your ARIMA params
-            virtual std::vector<double> predict(const std::vector<double>& col);
-
-            // Function to fir and predict based on your ARIMA params on your vector
-            virtual std::vector<double> fit_predict(const std::vector<double>& col);
-
-            // Getters
-            // Returns p, d, q
-            virtual std::vector<int> get_params() const { return {p_, d_, q_}; }
-            virtual const std::vector<double>& get_ydiff() const { return y_diff; }
-            virtual const std::vector<double>& get_arcoeffs() const { return ar_coeffs; }
-            virtual const std::vector<double>& get_arcoeffs() const { return ma_coeffs; }
-            virtual const std::vector<double>& get_residuals() const { return residuals; }
-    };
-
-    class SARIMA : public ARIMA {
-        private:
-            int P_;
-            int D_;
-            int Q_;
-            int s_;
-            bool seasonal_;
-
-        public:
-            // Set params to -1 for them to be automatically detected
-            SARIMA(int p = -1, int d = -1, int q = -1, int P = -1, int D = -1, int Q = -1, int s = -1) :
-                ARIMA(p, d, q), P_(P), D_(D), Q_(Q), s_(s) {};
-
-            // Function to detect and returns SARIMA params on your vector
-            // Will return p, d, q, P, D, Q, s and seasonality 
-            std::vector<int> detect(const std::vector<double>& col) override;
-
-            // Function to detect and returns SARIMA params on your vector
-            void fit(const std::vector<double>& col) override;
-
-            // Function to predict based on your SARIMA params
-            std::vector<double> predict(const std::vector<double>& col) override;
-
-            // Function to fir and predict based on your SARIMA params on your vector
-            std::vector<double> fit_predict(const std::vector<double>& col) override;
-
-            // Setter
-            void set_seasonal(bool seasonal) { seasonal_ = seasonal; }
-
-            // Getters
-            // Returns p, d, q, P, D, Q, s and seasonal
-            std::vector<int> get_params() const override { return {p_, d_, q_, P_, D_, Q_, s_, seasonal_ ? 1 : 0}; }
-    };
+    // Function to detect and returns ARIMA params on your vector
+    // Will return p, d, q, P, D, Q, s and seasonality
+    std::vector<int> detect_SARIMA(const std::vector<double>& col);
 
     // FFT (Fast Fourier Transformation)
     int Fft(const std::vector<double>& y);
@@ -80,14 +18,23 @@ namespace Stats_TS {
     // Seasonality test Kruskal_Wallis
     bool Kruskal_Wallis(const std::vector<double>& y, int s);
 
+    // Linear detrend for Kurskal_Wallis
+    std::vector<double> linear_detrend(const std::vector<double>& y);
+
     // ACF to find period (seasonality), will return s
     int Acf_s(const std::vector<double>& y);
+
+    // ACF_s will return Q
+    int Acf_s_q(const std::vector<double>& y, int s);
 
     // ACF, will return q (MA)
     int Acf(const std::vector<double>& y);
 
     // PACF through Durbin-Levinson, will return p (AR)
     int Pacf(const std::vector<double>& y);
+
+    // PACF_s through Durbin-Levinson, will return P
+    int Pacf_s(const std::vector<double>& y, int s);
 
     // Augmented Dickey-Fuller test with y, will return adf_stat for stationarity
     double ADF_test(const std::vector<double>& y);
--- src/Stats/Time_series.cpp
+++ src/Stats/Time_series.cpp
@@ -5,160 +5,118 @@
 #include "Stats/stats_reg.hpp"
 #include "Stats/Time_series.hpp"
 #include <boost/math/distributions/chi_squared.hpp>
-#include "Models/Supervised/Regression/LinearReg.hpp"
+#include "Models/Supervised/Regression/RidgeReg.hpp"
 
 using namespace Utils;
 
 namespace Stats_TS {
 
-std::vector<int> ARIMA::detect(const std::vector<double>& col) {
+std::vector<int> detect_ARIMA(const std::vector<double>& col) {
+    int p;
+    int q;
     std::vector<double> y;
 
     // Detect d (stationarity)
-    if (d_ == -1) {
-        int d = 0;
-        bool keep_cond = true;
-        y = col;
-        while (keep_cond && d < 3) {
+    int d = 0;
+    bool keep_cond = true;
+    y = col;
+    while (keep_cond && d < 3) {
 
-            // Diff
-            if (d > 0) {
-                std::vector<double> diff;
-                diff.reserve(y.size() - 1);
-                for (size_t i = 1; i < y.size(); i++) {
-                    diff.push_back(y[i] - y[i-1]);
-                }   
-                y = diff;
-            }
-
-            // ADF test
-            double adf_stat = ADF_test(y);
-            double cv = critical_value_MacKinon(y.size());
-            if (adf_stat < cv) {
-                keep_cond = false;
-            }
-            else if (d == 2) {
-                d = -2;
-                break;
-            }
-            else d++;
-        }
-        d_ = d;
-        if (d_ == -2) {
-            p_ = -2;
-            q_ = -2;
-        }
-    }
-
-    // Detect p (AR)
-    if (p_ == -1) {
-
-        // If d_ from user input
-        if (y.empty()) {
-            y = col;
-            for (size_t i = 0; i < d_; i++) {
-
-                // Diff
-                std::vector<double> diff;
-                diff.reserve(y.size() - 1);
-                for (size_t j = 1; j < y.size(); j++) {
-                    diff.push_back(y[j] - y[j-1]);
-                }   
-                y = diff;
-            }
-        }
-        p_ = Pacf(y);
-    }
-
-    // Detect q (MA)
-    if (q_ == -1) {
-
-        // If d_ from user input
-        if (y.empty()) {
-            y = col;
-            for (size_t i = 0; i < d_; i++) {
-
-                // Diff
-                std::vector<double> diff;
-                diff.reserve(y.size() - 1);
-                for (size_t j = 1; j < y.size(); j++) {
-                    diff.push_back(y[j] - y[j-1]);
-                }   
-                y = diff;
-            }
-        }
-        q_ = Acf(y);
-    }
-    y_diff = y;
-    return {p_, d_, q_};
-}
-
-void ARIMA::fit(const std::vector<double>& col) {
-
-    // To handle if not detected before (params on auto)
-    detect(col);
-    if (y_diff.empty()) {
-        std::vector<double> y = col;
-        for (size_t i = 0; i < d_; i++) {
-
-            // Diff
+        // Diff
+        if (d > 0) {
             std::vector<double> diff;
             diff.reserve(y.size() - 1);
-            for (size_t j = 1; j < y.size(); j++) {
-                diff.push_back(y[j] - y[j-1]);
+            for (size_t i = 1; i < y.size(); i++) {
+                diff.push_back(y[i] - y[i-1]);
             }   
             y = diff;
         }
-        y_diff = y;
-    }
 
-    if (p_ > 0) {
-        int n = y_diff.size() - p_; 
-
-        // Build X and y_target
-        std::vector<double> X_v(n * p_);
-        std::vector<double> y_target(n);
-
-        for (size_t i = 0; i < n; i++) {
-
-            y_target[i] = y_diff[i + p_];
-            for (int j = 0; j < p_; j++) {
-                X_v[j * n + i] = y_diff[i + p_ - 1 - j];
-            }
+        // ADF test
+        double adf_stat = ADF_test(y);
+        double cv = critical_value_MacKinon(y.size());
+        if (adf_stat < cv) {
+            keep_cond = false;
         }
-
-        // Input
-        Dataframe X = {n, p_, false, std::move(X_v)};
-        Dataframe Y = {y_target.size(), 1, false, std::move(y_target)};
-        Reg::LinearRegression Ar_reg;
-        
-        // OLS Reg
-        Ar_reg.fit_without_stats(X, Y);
-        ar_coeffs = Ar_reg.get_coeffs();
+        else if (d == 2) {
+            d = -2;
+            break;
+        }
+        else d++;
+    }
+    if (d == -2) {
+        p = -2;
+        q = -2;
+        return {p, d, q};
     }
 
-    if (q_ > 0) {
-        residuals = compute_residuals(y_diff, ar_coeffs);
-        ma_coeffs = fit_MA(residuals, q_);
-    }
+    // Detect p (AR)
+    p = Pacf(y);
 
+    // Detect q (MA)
+    q = Acf(y);
+
+    return {p, d, q};
 }
 
-void SARIMA::fit(const std::vector<double>& col) {
+std::vector<int> detect_SARIMA(const std::vector<double>& col) {
 
-    std::vector<double> y;
+    bool seasonal = true;
+    std::vector<double> y = col;
 
     // Detect a period with ACF and first approx of period s
-    s_ = Acf_s(col);
+    int s = Acf_s(y);
 
     // Testing if we have or not seasonality
-    if (Kruskal_Wallis(col, s_)) {
-        s_ = Fft(col);
+    std::vector<double> y_det = linear_detrend(y);
+    if (Kruskal_Wallis(y_det, s) && s != 0) {
+        s = Fft(y_det);
     }
     else {
-        s_ = 0;
-        seasonal_ = false;
+        s = 0;
+        seasonal = false;
     }
+
+    if (!seasonal) {
+        // Fallback sur ARIMA classique
+        std::vector<int> arima = detect_ARIMA(col);
+        return {arima[0], arima[1], arima[2], 0, 0, 0, 0, 0};
+    }
+
+    // Detect D
+    int D = 0;
+    bool keep_cond = true;
+    while (keep_cond && D < 2) {
+
+        if (D > 0) {
+
+            std::vector<double> diff;
+            diff.reserve(y.size() - s);
+            for (size_t i = s; i < y.size(); i++) {
+                diff.push_back(y[i] - y[i - s]);
+            }
+            y = diff;
+        }
+
+        double adf_stat = ADF_test(y);
+        double cv = critical_value_MacKinon(y.size());
+        if (adf_stat < cv) {
+            keep_cond = false;
+        } else {
+            D++;
+        }
+    }
+
+    // Detect P and Q
+    int P = Pacf_s(y, s);
+    int Q = Acf_s_q(y, s);
+
+    // Detect p, d, q on our y
+    std::vector<int> arima = detect_ARIMA(y);
+    int p = arima[0], d = arima[1], q = arima[2];
+
+    // (p,d,q)(P,D,Q,s,seasonal)
+    return {p, d, q, P, D, Q, s, seasonal ? 1 : 0};
 }
 
 int Fft(const std::vector<double>& y) {
@@ -174,19 +132,19 @@ int Fft(const std::vector<double>& y) {
     std::complex<double> imag(0, 1);
     for (size_t i = 0; i < nb_val; i++) {
         
-        double f = i / n;
+        double f = static_cast<double>(i) / n;
         std::complex<double> x = 0.0;
         for (size_t j = 0; j < n; j++) {
-            x += y[j] * std::exp(-2.0 * pi * imag * (double)f * (double)j);
+            x += y[j] * std::exp(-2.0 * pi * imag * f * static_cast<double>(j));
         }
-        P.push_back(std::norm(x) / n);
+        P.push_back(std::norm(x));
     }
 
     // Getting idx of max element
     size_t it = std::max_element(P.begin() + 1, P.end()) - P.begin();
 
-    // s
-    return static_cast<int>(it);
+    double freq = static_cast<double>(it) / n;
+    return static_cast<int>(std::round(1.0 / freq));
 }
 
 bool Kruskal_Wallis(const std::vector<double>& y, int s) {
@@ -200,21 +158,75 @@ bool Kruskal_Wallis(const std::vector<double>& y, int s) {
     std::vector<double> R(s, 0.0);
     std::vector<int> n_i(s, 0);
     
-    for (size_t t = 0; t < n; t++) {
-        int group = t % s;
-        R[group] += ranks[t];
+    for (size_t i = 0; i < n; i++) {
+        int group = i % s;
+        R[group] += ranks[i];
         n_i[group]++;
     }
 
     // Calculating H 
     double H = 0.0;
     for (size_t i = 0; i < s; i++) H += (R[i] * R[i]) / n_i[i];
-    H *= 12 / (n *(n + 1));
+    H *= 12.0 / (n * (n + 1));
     H -= 3 * (n + 1);
 
+    if (s <= 1) return false;
     boost::math::chi_squared dist(s - 1);
     double p_value = 1.0 - boost::math::cdf(dist, H);
     return p_value < 0.05;
+}
+
+int Acf_s_q(const std::vector<double>& y, int s) {
+
+    size_t n = y.size();
+    double mean_y = Stats::mean(y);
+    size_t kmax = n / 4 > 40 ? 40 : n / 4;
+    size_t Qmax = kmax / s;
+    if (Qmax < 1) return 0;
+
+    double gamma_0;
+    double threshold = 1.96 / std::sqrt(n);
+    int Q = 0;
+
+    for (size_t k = 0; k <= Qmax; k++) {
+
+        size_t lag = k * s;
+        double gamma = 0.0;
+        for (size_t i = lag; i < n; i++) {
+            gamma += (y[i] - mean_y) * (y[i - lag] - mean_y);
+        }
+        gamma /= n;
+        if (k == 0) gamma_0 = gamma;
+
+        double rho = gamma / gamma_0;
+
+        if ((std::abs(rho) <= threshold) && (k > 0)) {
+            Q = k - 1;
+            break;
+        }
+    }
+    return Q;
+}
+
+std::vector<double> linear_detrend(const std::vector<double>& y) {
+    size_t n = y.size();
+    double sx = 0, sy = 0, sxx = 0, sxy = 0;
+    
+    for (size_t i = 0; i < n; i++) {
+        sx  += i;
+        sy  += y[i];
+        sxx += i * i;
+        sxy += i * y[i];
+    }
+    
+    double c = n * sxx - sx * sx;
+    double b = (n * sxy - sx * sy) / c;
+    double a = (sy - b * sx) / n;
+    
+    std::vector<double> y_det(n);
+    for (size_t i = 0; i < n; i++)
+        y_det[i] = y[i] - (a + b * i);
+    return y_det;
 }
 
 int Acf_s(const std::vector<double>& y) {
@@ -235,10 +247,7 @@ int Acf_s(const std::vector<double>& y) {
         }   
         gamma /= n;
         if (k == 0) gamma_0 = gamma;
-
-        // If significant 
-        double rho_ = gamma / gamma_0;
-        if (std::abs(rho_) > threshold) rho[k] = rho_;
+        rho[k] = gamma / gamma_0;
     }
 
     // Finding patterns within significant acf stats
@@ -246,9 +255,8 @@ int Acf_s(const std::vector<double>& y) {
     for (size_t i = 4; i <= kmax; i++) {
 
         // If significant value
-        if (std::abs(rho[i]) > 0) {
-        if (i*2 <= kmax && i*3 <= kmax)
-            if (rho[i*2] > 0 && rho[i*3] > 0 && rho[i*2] < rho[i] && rho[i*3] < rho[i*2]) {
+        if (std::abs(rho[i]) > threshold) {
+            if (rho[i-1] < rho[i] && rho[i+1] < rho[i]) {
                 s = static_cast<int>(i);
                 break;
             }
@@ -332,19 +340,74 @@ int Pacf(const std::vector<double>& y) {
         }
 
         // Test
-        if (std::abs(phi[k][k]) <= threshold) {
-            p = k - 1;
+        if (std::abs(phi[k][k]) > threshold) {
+            p = k;
+        } else {
             break;
         }
     }
     return p;
 }
 
+int Pacf_s(const std::vector<double>& y, int s) {
+
+    size_t n = y.size();
+    double mean_y = Stats::mean(y);
+    size_t kmax = n / 4 > 40 ? 40 : n / 4;
+    size_t Pmax = kmax / s;
+    if (Pmax < 1) return 0;
+
+    std::vector<std::vector<double>> phi(Pmax + 1, std::vector<double>(Pmax + 1, 0.0));
+
+    // ACF aux lags multiples de s
+    double gamma_0;
+    std::vector<double> rho(Pmax + 1, 0.0);
+    for (size_t k = 0; k <= Pmax; k++) {
+        
+        size_t lag = k * s;
+        double gamma = 0.0;
+        for (size_t i = lag; i < n; i++) {
+            gamma += (y[i] - mean_y) * (y[i - lag] - mean_y);
+        }
+        gamma /= n;
+        if (k == 0) gamma_0 = gamma;
+        rho[k] = gamma / gamma_0;
+    }
+
+    // Durbin-Levinson sur ces lags
+    int P = 0;
+    phi[1][1] = rho[1];
+    double threshold = 1.96 / std::sqrt(n);
+
+    if (std::abs(phi[1][1]) > threshold) P = 1;
+
+    for (size_t k = 2; k <= Pmax; k++) {
+
+        double sum0 = 0.0, sum1 = 0.0;
+        for (size_t j = 1; j <= k-1; j++) {
+            sum0 += phi[k-1][j] * rho[k-j];
+            sum1 += phi[k-1][j] * rho[j];
+        }
+        phi[k][k] = (rho[k] - sum0) / (1 - sum1);
+
+        for (size_t j = 1; j <= k-1; j++) {
+            phi[k][j] = phi[k-1][j] - phi[k][k] * phi[k-1][k-j];
+        }
+
+        if (std::abs(phi[k][k]) > threshold) {
+            P = k;
+        } else {
+            break;
+        }
+    }
+    return P;
+}
+
 double ADF_test(const std::vector<double>& y) {
 
     // Schwert rule
     size_t n = y.size();
-    int p = (int)(12 * std::pow(n / 100.0, 0.25));
+    int p = static_cast<int>(12 * std::pow(n / 100.0, 0.25));
     size_t nb_cols = 3 + p;
 
     // Calculate our lags
@@ -375,16 +438,16 @@ double ADF_test(const std::vector<double>& y) {
     Dataframe X = {(n - p - 1), nb_cols, false, std::move(delta_x)};
     Dataframe Y = {(n - p - 1), 1, false, std::move(y_v)};
     
-    // Linear Reg
-    Reg::LinearRegression LinReg; 
-    auto [X_const, XtXinv] = LinReg.fit_without_stats(X, Y);
+    // Ridge Reg
+    Reg::RidgeRegression RiReg(1e-10);
+    auto [X_const, XtXinv] = RiReg.fit_without_stats(X, Y);
 
     // Predict and residuals
-    std::vector<double> y_pred = LinReg.predict(X);
+    std::vector<double> y_pred = RiReg.predict(X);
     std::vector<double> residuals = Stats::get_residuals(Y.get_data(), y_pred);
     
     // Getting data 
-    double gamma = LinReg.get_coeffs()[2];
+    double gamma = RiReg.get_coeffs()[3];
     double se_gamma = Stats::OLS::stderr_b(residuals, XtXinv)[2];
     double adf_stat = gamma / se_gamma;
 
